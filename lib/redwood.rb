18673a1d8bfbcfb18ade28233e68fa474d3e437b
Mark Wunsch <mark@markwunsch.com>
2010-04-14 09:54:51 -0400

    Version bump v0.1.2

--- lib/redwood.rb
+++ lib/redwood.rb
@@ -4,7 +4,7 @@
 # also mixes in tree-like methods. See Redwood::Node for the canononical representation.
 
 module Redwood
-  VERSION = "0.1.1" 
+  VERSION = "0.1.2" 
   
   # This node's parent.
   def parent
